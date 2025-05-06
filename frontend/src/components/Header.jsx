5cb0a2611c9f20900a69296dc029ac8eb19854ec
GiuseppeFiliberto <giuseppe.filiberto.jobs@gmail.com>
2025-05-06 12:30:35 +0200

    add Header component and DefaultLayout for improved navigation structure

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,46 @@
+
+export default function Header() {
+
+    return (
+        <>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">BoolShop</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Shop</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">About</a>
+                            </li>
+                        </ul>
+                        <form className="d-flex me-3">
+                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                            <button className="btn btn-outline-success" type="submit">Search</button>
+                        </form>
+                        <a href="#" className="btn btn-outline-primary position-relative">
+                            <i className="bi bi-cart"></i>
+                            <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                                0
+                                <span className="visually-hidden">cart items</span>
+                            </span>
+                        </a>
+                    </div>
+                </div>
+            </nav>
+        </>
+    )
+}
+
+
+
+
+
