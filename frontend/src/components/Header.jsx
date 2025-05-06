513eb214e389447d58553b741c7c9a1f55189208
NavodContrath <navod.contrath@gmail.com>
2025-05-06 17:13:25 +0200

    update Header component to use new logo; add Awlabpezzotto.webp and impact.png assets

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
         <>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#">BoolShop</a>
+                    <a className="navbar-brand" href="#"><img src="/assets/Awlabpezzotto.webp" width={'100px'} alt="kicks-logo" /></a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                         aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
