0efa650409a19c57be3d97ff6f64066878681300
GiuseppeFiliberto <giuseppe.filiberto.jobs@gmail.com>
2025-05-06 12:20:21 +0200

    add Home component and update App to use routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,13 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+
 export default function App() {
   return (
-    <>
-      <h1>test</h1>
-    </>
+
+    <Home>
+
+    </Home>
+
   );
 }
