450e47b3c04373ce1772839ba7268006212b302b
NavodContrath <navod.contrath@gmail.com>
2025-05-06 15:45:46 +0200

    add Footer component and integrate it into DefaultLayout; update styles in index.css

--- frontend/src/layout/DefaultLayout.jsx
+++ frontend/src/layout/DefaultLayout.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header"
+import Footer from "../components/Footer";
 
 export default function DefaultLayout() {
 
@@ -7,8 +8,10 @@ export default function DefaultLayout() {
         <>
 
             <Header />
-
-            <Outlet />
+            <main>
+                <Outlet />
+            </main>
+            <Footer />
 
 
         </>
