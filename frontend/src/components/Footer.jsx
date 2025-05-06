6c3c2bea55f998fd0767ffe0bb70c76c749dc606
NavodContrath <navod.contrath@gmail.com>
2025-05-06 16:48:13 +0200

    update Footer component styles and logo; change link colors to black

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,17 +3,17 @@ export default function Footer() {
     return (
         <>
 
-            <div className="foot-bar bg-black">
+            <div className="foot-bar bg-whitesmoke">
                 <div className="container d-flex justify-content-between align-items-center">
 
                     <div className="foot-logo">
-                        <img src="impact.png" width={'150px'} alt="impact-logo" />
+                        <img src="/assets/01.webp" width={'150px'} alt="kicks-logo" />
                     </div>
                     <div className="foot-socials">
                         <ul className="list-unstyled d-flex gap-3">
-                            <li><a href=""><i className="bi bi-facebook text-light"></i></a></li>
-                            <li><a href=""><i className="bi bi-instagram text-light"></i></a></li>
-                            <li><a href=""><i className="bi bi-twitter-x text-light"></i></a></li>
+                            <li><a href=""><i className="bi bi-facebook"></i></a></li>
+                            <li><a href=""><i className="bi bi-instagram"></i></a></li>
+                            <li><a href=""><i className="bi bi-twitter-x"></i></a></li>
                         </ul>
                     </div>
                     <div className="foot-links my-3">
