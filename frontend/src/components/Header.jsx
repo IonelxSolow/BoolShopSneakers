14c0af76fc86015d5d75e485ecf44ce96cebab67
alexciccarese <alex.ciccarese@gmail.com>
2025-05-08 11:39:41 +0200

    feat: implement responsive menu toggle and enhance header styling

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,23 +1,30 @@
+import { useState } from "react"
 
 export default function Header() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen)
+    }
 
     return (
         <>
-            <header className="d-flex justify-content-center global_container mt-4">
-                <nav className="navbar custom-header py-3 px-4 bg-light mb-4 w-100">
-                    <div class="container-fluid d-flex justify-content-between align-items-center">
-                        <div className="row row-cols-1 row-cols-md-3 g-0 w-100">
-                            <div class="d-flex align-items-center gap-4">
-                                <a href="#" class="nav-link">New Drops 🔥</a>
-                                <a href="#" class="nav-link">Men ▼</a>
-                                <a href="#" class="nav-link">Women ▼</a>
-                            </div>
-                            <a class="navbar-brand mx-auto align-middle text-center" href="#">KICKS</a>
-                            <div class="d-flex justify-content-end align-items-center gap-3">
-                                <a href="#" class="nav-link"><i class="bi bi-search"></i></a>
-                                <a href="#" class="nav-link"><i class="bi bi-person"></i></a>
-                                <a href="#" class="nav-link icon-btn">0</a>
-                            </div>
+            <header>
+                <nav className="navbar custom-header py-3 px-2">
+                    <div className="container-fluid d-flex justify-content-between align-items-center">
+                        <button className="burger-menu d-md-none" onClick={toggleMenu}>
+                            <i className="bi bi-list"></i>
+                        </button>
+                        <div className={`menu-list d-md-flex ${isMenuOpen ? "open" : "d-none"}`}>
+                            <a href="#" className="nav-link">New Drops 🔥</a>
+                            <a href="#" className="nav-link">Men ▼</a>
+                            <a href="#" className="nav-link">Women ▼</a>
+                        </div>
+                        <a className="navbar-brand mx-auto text-center" href="#">KICKS</a>
+                        <div className="d-flex justify-content-end align-items-center gap-3">
+                            <a href="#" className="nav-link"><i className="bi bi-search"></i></a>
+                            <a href="#" className="nav-link"><i className="bi bi-person"></i></a>
+                            <a href="#" className="nav-link icon-btn">0</a>
                         </div>
                     </div>
                 </nav>
