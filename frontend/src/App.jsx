573ccdecf728555bc5794ed1f734a12344d66b59
imnxtamess <davide.morisco.work@gmail.com>
2025-05-08 14:46:37 +0200

    refactor: update component imports and improve code structure for consistency feat: enhance Home and SingleProduct components with improved navigation and layout style: standardize CSS variable definitions and improve styling consistency

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,21 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { GlobalProvider } from './context/GlobalContext';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { GlobalProvider } from "./context/GlobalContext";
 
-import Home from './pages/Home';
-import DefaultLayout from './layout/DefaultLayout';
-import SingleProduct from './pages/SingleProduct';
+import Home from "./pages/Home";
+import DefaultLayout from "./layout/DefaultLayout";
+import SingleProduct from "./pages/SingleProduct";
 
 export default function App() {
   return (
-
     <GlobalProvider>
       <BrowserRouter>
         <Routes>
           <Route element={<DefaultLayout />}>
-            <Route path='/' element={<Home />} />
-            <Route path='/sneakers/:slug' element={<SingleProduct />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/product/:slug" element={<SingleProduct />} />
           </Route>
         </Routes>
       </BrowserRouter>
     </GlobalProvider>
-
   );
 }
--- frontend/src/pages/SingleProduct.jsx
+++ frontend/src/pages/SingleProduct.jsx
@@ -1,9 +1,65 @@
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { useGlobalContext } from "../context/GlobalContext";
+
 export default function SingleProduct() {
+  const { sneakers, loading } = useGlobalContext();
+  const { slug } = useParams();
+  const [productId, setProductId] = useState("");
+  const [product, setProduct] = useState(null);
 
-  return(
+  function getSneakerId() {
+    if (loading === true) {
+      console.log("Fetch didn't load"); // can implement possible 404 logic here
+      return;
+    }
+    const sneaker = sneakers.find((sneaker) => {
+      if (sneaker.name.toLowerCase().replaceAll(" ", "-") === slug) {
+        return sneaker;
+      }
+    });
+    if (sneaker) {
+      setProductId(sneaker.id);
+    }
+  }
 
+  useEffect(() => {
+    getSneakerId();
+    fetch(`http://localhost:3000/boolshop/api/v1/shoes/3`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setProduct(data);
+      });
+  }, [productId]);
+
+  return (
     <>
-    <h1>sneaker</h1>
+      <div className="container single-page">
+        <div className="row">
+          <div className="col-2">
+            <div className="thumbContainer">
+              <p>hello im the thumbcontainer</p>
+            </div>
+          </div>
+          <div className="col-5">
+            <div className="carouselContainer">
+              <div className="carousel">
+                <img
+                  className="img-fluid"
+                  src="/images/Scarpe/04_New_Balance/05_2002R/04_05_Variant_1/05_NewBalance2002R_01.webp"
+                  alt=""
+                />
+              </div>
+            </div>
+          </div>
+          <div className="col-5">
+            <div className="detailsContainer">
+              <p>hello im the details Container</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
