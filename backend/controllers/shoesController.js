bef8cf6e34efd5049994afc57e1c75d8cfb09ac2
NavodContrath <navod.contrath@gmail.com>
2025-05-09 11:30:36 +0200

    add variant image

--- backend/controllers/shoesController.js
+++ backend/controllers/shoesController.js
@@ -17,6 +17,7 @@ function index(req, res) {
       GROUP_CONCAT(DISTINCT variants.size) AS variant_sizes,
       GROUP_CONCAT(DISTINCT variants.color) AS variant_colors,
       GROUP_CONCAT(DISTINCT variants.stock) AS variant_stocks,
+      GROUP_CONCAT(DISTINCT variants.image_urls) AS variant_images,
       GROUP_CONCAT(DISTINCT shoe_images.url) AS image_urls,
       GROUP_CONCAT(DISTINCT tags.name) AS tags
     FROM shoes
@@ -26,9 +27,7 @@ function index(req, res) {
       LEFT JOIN tags ON tags.id = shoe_tags.tag_id
       LEFT JOIN discounts ON discounts.id = shoes.discount_id
     GROUP BY shoes.id;
-
 `
-
   connection.query(sql, [], (err, results) => {
     if (err) res.status(500).json({ message: err.message })
     if (results.length === 0) return res.status(404).json({ error: 'sneakers not found' })
