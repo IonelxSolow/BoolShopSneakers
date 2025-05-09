a42b938adbd8508c7f122d3368194dc15a14288d
alexciccarese <alex.ciccarese@gmail.com>
2025-05-09 10:58:38 +0200

    feat: add responsive menu toggle and adjust header padding

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,24 +1,40 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 export default function Header() {
     const [isMenuOpen, setIsMenuOpen] = useState(false)
 
     const toggleMenu = () => {
-        setIsMenuOpen(!isMenuOpen)
-    }
+        setIsMenuOpen(!isMenuOpen);
+    };
+
+    useEffect(() => {
+        const handleResize = () => {
+            if (window.innerWidth > 768) {
+                setIsMenuOpen(false);
+            }
+        };
+
+        window.addEventListener("resize", handleResize);
+
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
 
     return (
         <>
             <header>
-                <nav className="navbar custom-header py-3 px-2">
+                <nav className="navbar custom-header py-3">
                     <div className="container d-flex justify-content-between align-items-center">
                         <button className="burger-menu d-md-none" onClick={toggleMenu}>
                             <i className="bi bi-list"></i>
                         </button>
                         <div className={`menu-list d-md-flex ${isMenuOpen ? "open" : "d-none"}`}>
-                            <a href="#" className="nav-link">New Drops 🔥</a>
-                            <a href="#" className="nav-link">Men ▼</a>
-                            <a href="#" className="nav-link">Women ▼</a>
+                            <div className="d-flex flex-column flex-md-row gap-3">
+                                <a href="#" className="nav-link container">Men</a>
+                                <a href="#" className="nav-link container">Women</a>
+                                <a href="#" className="nav-link container">Kids</a>
+                            </div>
                         </div>
                         <a className="navbar-brand mx-auto text-center fw-bolder" href="#">
                             <h4 className="fw-bold">KICKSOCIETY</h4>
