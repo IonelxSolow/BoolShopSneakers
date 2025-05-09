a8a9d82b2c4e594c521550d75bc6cdce5f2aa79f
NavodContrath <navod.contrath@gmail.com>
2025-05-09 11:05:10 +0200

    fix: improve SQL queries and add variants to sneaker response

--- backend/routers/shoesRouter.js
+++ backend/routers/shoesRouter.js
@@ -2,11 +2,10 @@ const router = require('express').Router()
 const shoesController = require('../controllers/shoesController')
 
 // get all shoes 
-
 router.get('/', shoesController.index)
-
+//get single shoe
 router.get('/:id', shoesController.show)
-
+//update sold copies
 router.patch('/:id/sold', shoesController.updateSoldCopies)
 
 module.exports = router
