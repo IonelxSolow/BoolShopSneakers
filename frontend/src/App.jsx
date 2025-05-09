f6c758961fa261ca48a780bda0b2bddc02a0dc90
alexciccarese <alex.ciccarese@gmail.com>
2025-05-09 12:05:25 +0200

    fix: resolve merge conflicts and clean up imports in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,13 +4,10 @@ import { GlobalProvider } from "./context/GlobalContext";
 import Home from "./pages/Home";
 import DefaultLayout from "./layout/DefaultLayout";
 import SingleProduct from "./pages/SingleProduct";
-<<<<<<< HEAD
 import Men from "./pages/Men";
 import Women from "./pages/Women";
 import Kids from "./pages/Kids";
-=======
 import AllProducts from "./pages/AllProducts";
->>>>>>> 477771a6cb74f011b1d60b7b40460056988a0278
 
 export default function App() {
   return (
@@ -20,13 +17,10 @@ export default function App() {
           <Route element={<DefaultLayout />}>
             <Route path="/" element={<Home />} />
             <Route path="/product/:slug" element={<SingleProduct />} />
-<<<<<<< HEAD
             <Route path="/men" element={<Men />} />
             <Route path="/women" element={<Women />} />
             <Route path="/kids" element={<Kids />} />
-=======
             <Route path="/all-products" element={<AllProducts />} />
->>>>>>> 477771a6cb74f011b1d60b7b40460056988a0278
           </Route>
         </Routes>
       </BrowserRouter>
