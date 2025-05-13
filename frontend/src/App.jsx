619381cddc1e000db6711ba67317849a0b967bc1
alexciccarese <alex.ciccarese@gmail.com>
2025-05-13 21:57:25 +0200

    fix: correct typo in WishlistProvider import path

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,35 +14,38 @@ import CartPage from "./pages/CartPage";
 import Checkout from "./pages/Checkout";
 import OrderConfirmation from "./pages/OrderConfirmation";
 import OrderDetails from "./pages/OrderDetails";
+import { WishlistProvider } from "./context/WhishlistContext";
 
 export default function App() {
   return (
     <GlobalProvider>
       <SearchProvider>
-        <CartProvider>
-          <BrowserRouter>
-            <Routes>
-              <Route element={<DefaultLayout />}>
-                <Route path="/" element={<Home />} />
-                <Route path="/product/:slug" element={<SingleProduct />} />
-                <Route path="/men" element={<Men />} />
-                <Route path="/women" element={<Women />} />
-                <Route path="/kids" element={<Kids />} />
-                <Route path="/all-products?" element={<AllProducts />} />
-                <Route path="/cart" element={<CartPage />} />
-                <Route path="/checkout" element={<Checkout />} />
-                <Route
-                  path="/order-confirmation"
-                  element={<OrderConfirmation />}
-                />
-                <Route
-                  path="/account/orders/:orderId"
-                  element={<OrderDetails />}
-                />
-              </Route>
-            </Routes>
-          </BrowserRouter>
-        </CartProvider>
+        <WishlistProvider>
+          <CartProvider>
+            <BrowserRouter>
+              <Routes>
+                <Route element={<DefaultLayout />}>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/product/:slug" element={<SingleProduct />} />
+                  <Route path="/men" element={<Men />} />
+                  <Route path="/women" element={<Women />} />
+                  <Route path="/kids" element={<Kids />} />
+                  <Route path="/all-products?" element={<AllProducts />} />
+                  <Route path="/cart" element={<CartPage />} />
+                  <Route path="/checkout" element={<Checkout />} />
+                  <Route
+                    path="/order-confirmation"
+                    element={<OrderConfirmation />}
+                  />
+                  <Route
+                    path="/account/orders/:orderId"
+                    element={<OrderDetails />}
+                  />
+                </Route>
+              </Routes>
+            </BrowserRouter>
+          </CartProvider>
+        </WishlistProvider>
       </SearchProvider>
     </GlobalProvider>
   );
