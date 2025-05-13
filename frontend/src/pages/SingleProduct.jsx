08fa3d3d6a9c3d61558d7a304b6300f2d592582c
imnxtamess <davide.morisco.work@gmail.com>
2025-05-13 14:16:35 +0200

    style: clean up CSS formatting and improve structure; enhance suggested items display in SingleProduct component

--- frontend/src/pages/SingleProduct.jsx
+++ frontend/src/pages/SingleProduct.jsx
@@ -380,18 +380,39 @@ export default function SingleProduct() {
             </h1>
             <div className="suggestedItemsContainer d-flex pt-3 pb-4 px-3 gap-4">
               {suggestedItems.slice(1, 5).map((sneaker, index) => (
-                <div className="suggestedItemWrapper">
-                  <Link
-                    to={`/product/${sneaker.name
-                      .toLowerCase()
-                      .replaceAll(" ", "-")}`}
-                  >
-                    <img
-                      className="img-fluid"
-                      src={`/assets/${parsedImages[index][0]}`}
-                      alt=""
-                    />
-                  </Link>
+                <div className="suggestedItem">
+                  <div className="suggestedItemWrapper">
+                    <Link
+                      to={`/product/${sneaker.name
+                        .toLowerCase()
+                        .replaceAll(" ", "-")}`}
+                    >
+                      <img
+                        className="img-fluid"
+                        src={`/assets/${parsedImages[index][0]}`}
+                        alt=""
+                      />
+                    </Link>
+                  </div>
+                  <div className="pt-2">
+                    <h4 className="fw-bold text-uppercase ">
+                      <Link
+                        to={`/product/${sneaker.name
+                          .toLowerCase()
+                          .replaceAll(" ", "-")}`}
+                      >
+                        {sneaker.name}
+                      </Link>
+                    </h4>
+                    <h6>
+                      <Link
+                        to={`/all-products?brand=${sneaker.brand}`}
+                        className="text-secondary"
+                      >
+                        {sneaker.brand}
+                      </Link>
+                    </h6>
+                  </div>
                 </div>
               ))}
             </div>
