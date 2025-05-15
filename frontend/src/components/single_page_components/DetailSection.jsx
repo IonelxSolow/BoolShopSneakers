790d5471749d17e4487ea750c32dbaa5497868d0
imnxtamess <davide.morisco.work@gmail.com>
2025-05-15 11:47:12 +0200

    refactor: enhance DetailSection component with wishlist functionality and improve layout

--- frontend/src/components/single_page_components/DetailSection.jsx
+++ frontend/src/components/single_page_components/DetailSection.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function DetailSection({
   product,
@@ -18,8 +18,8 @@ export default function DetailSection({
 }) {
   const [showToast, setShowToast] = useState(false);
   const [showToastWish, setShowToastWish] = useState(false);
-
   const [showErrorToast, setShowErrorToast] = useState(false);
+  const [isWishlist, setIsWishlist] = useState(null);
 
   function handleAddToCart() {
     if (activeIndex === null || activeIndex === undefined) {
@@ -38,15 +38,35 @@ export default function DetailSection({
       return;
     }
     addToWishList();
+    setIsWishlist(true);
     setShowToastWish(true);
-    setTimeout(() => setShowToast(false), 5000);
+    setTimeout(() => setShowToastWish(false), 5000);
   }
+
+  useEffect(() => {
+    const wishlistItems = JSON.parse(localStorage.getItem("wishlist")) || [];
+    setIsWishlist(wishlistItems.includes(product.result.sku));
+  }, [product.result.sku]);
+
+  console.log(isWishlist);
 
   return (
     <>
       <div className="col-12 col-xxl-3 order-3 d-flex">
         <div className="d-flex flex-column detailsContainer justify-content-between">
-          <h1 className="fw-bold text-uppercase">{product.result.name}</h1>
+          <div className="d-flex justify-content-between align-items-center">
+            <h1 className="fw-bold text-uppercase mb-0">
+              {product.result.name}
+            </h1>
+            <button className="btn fs-5" onClick={handleAddToWishList}>
+              {isWishlist ? (
+                <i className="bi bi-heart-fill text-danger"></i>
+              ) : (
+                <i className="bi bi-heart"></i>
+              )}
+            </button>
+          </div>
+
           <h2>
             <Link
               to={`/all-products?brand=${product.result.brand}`}
@@ -56,7 +76,7 @@ export default function DetailSection({
             </Link>
           </h2>
           {!product.result.discounted_price ||
-            parseFloat(product.result.discounted_price) >=
+          parseFloat(product.result.discounted_price) >=
             parseFloat(product.result.price) ? (
             <p className="text-dark">
               {parseFloat(product.result.price).toFixed(2)}&#8364;
@@ -84,8 +104,9 @@ export default function DetailSection({
           <div className="d-flex gap-2 circle-thumbs-container align-items-center flex-wrap">
             {" "}
             <div
-              className={`circle-thumb-wrapper ${variant === 0 && " active-link"
-                }`}
+              className={`circle-thumb-wrapper ${
+                variant === 0 && " active-link"
+              }`}
             >
               {images && (
                 <img
@@ -98,8 +119,9 @@ export default function DetailSection({
             </div>
             <div>
               <div
-                className={`circle-thumb-wrapper ${variant === 1 && " active-link"
-                  }`}
+                className={`circle-thumb-wrapper ${
+                  variant === 1 && " active-link"
+                }`}
               >
                 {variantImages && (
                   <img
@@ -118,40 +140,41 @@ export default function DetailSection({
           <div className="sizes-container d-flex gap-3 flex-wrap">
             {variant === 0
               ? mainSizes.map((size, index) => (
-                <div
-                  key={index}
-                  onClick={() => handleSizeClick(index)}
-                  className={`size-badge ${activeIndex === index && "active-link"
+                  <div
+                    key={index}
+                    onClick={() => handleSizeClick(index)}
+                    className={`size-badge ${
+                      activeIndex === index && "active-link"
                     }`}
-                >
-                  {size}
-                </div>
-              ))
+                  >
+                    {size}
+                  </div>
+                ))
               : variantSizes.map((size, index) => (
-                <div
-                  key={index}
-                  onClick={() => handleSizeClick(index)}
-                  className={`size-badge ${activeIndex === index && "active-link"
+                  <div
+                    key={index}
+                    onClick={() => handleSizeClick(index)}
+                    className={`size-badge ${
+                      activeIndex === index && "active-link"
                     }`}
-                >
-                  {size}
-                </div>
-              ))}
+                  >
+                    {size}
+                  </div>
+                ))}
           </div>
           <p className="my-2">
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Atque
             iste, quaerat iusto obcaecati suscipit modi veniam ipsam ipsum ex
             provident.
           </p>
+
           <button
             onClick={handleAddToCart}
             className="btn btn-main-light rounded-4 fs-4 my-3"
           >
             Add to Cart
           </button>
-          <div
-            onClick={handleAddToWishList}
-            className="btn btn-main-light rounded-4 fs-4 my-3">love</div>
+
           {showToast && (
             <div
               style={{
@@ -190,8 +213,8 @@ export default function DetailSection({
               </span>
             </div>
           )}
-          {
-            showToastWish && (<div
+          {showToastWish && (
+            <div
               style={{
                 position: "fixed",
                 bottom: 40,
@@ -226,8 +249,8 @@ export default function DetailSection({
               <span style={{ fontSize: 22, color: "#757575" }}>
                 Prodotto aggiunto alla wish list
               </span>
-            </div>)
-          }
+            </div>
+          )}
           {showErrorToast && (
             <div
               style={{
