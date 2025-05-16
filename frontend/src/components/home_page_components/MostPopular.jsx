ae064b9aa91912589a78704eeeeb5f2a7018ee39
imnxtamess <davide.morisco.work@gmail.com>
2025-05-16 16:37:15 +0200

    feat: implement counter for sneaker image rotation and update styles for improved responsiveness

--- frontend/src/components/home_page_components/MostPopular.jsx
+++ frontend/src/components/home_page_components/MostPopular.jsx
@@ -1,8 +1,22 @@
 import { useGlobalContext } from "../../context/GlobalContext";
 import { Link } from "react-router-dom";
-
+import { useEffect, useState } from "react";
 export default function MostPopular() {
   const { sneakers } = useGlobalContext();
+
+  const [counter, setCounter] = useState(1); // Convert to state
+
+  useEffect(() => {
+    setInterval(() => {
+      setCounter((prev) => {
+        if (prev < 28) {
+          return prev + 1;
+        }
+
+        return 1;
+      });
+    }, 100);
+  }, []);
 
   switch (sneakers.state) {
     case "loading":
@@ -24,11 +38,9 @@ export default function MostPopular() {
         }
       });
 
-      const parsedImg = JSON.parse(mostSoldSneaker.image_urls)[0];
-
       return (
         <>
-          <svg
+          {/* <svg
             className="wave-1hkxOo"
             version="1.1"
             xmlns="http://www.w3.org/2000/svg"
@@ -40,16 +52,20 @@ export default function MostPopular() {
               d="M826.337463,25.5396311 C670.970254,58.655965 603.696181,68.7870267 447.802481,35.1443383 C293.342778,1.81111414 137.33377,1.81111414 0,1.81111414 L0,150 L1920,150 L1920,1.81111414 C1739.53523,-16.6853983 1679.86404,73.1607868 1389.7826,37.4859505 C1099.70117,1.81111414 981.704672,-7.57670281 826.337463,25.5396311 Z"
               fill="currentColor"
             ></path>
-          </svg>
-          <section className="bg-dark p-0 container-fluid hot-item">
+          </svg> */}
+          <section className=" p-0 container-fluid hot-item">
             <div className="slider360 text-center">
               <img
                 className="image360"
-                src={`/360/jd_030664_spin_01-removebg-preview.png`}
+                src={
+                  counter < 10
+                    ? `/360/jd_030664_spin_0${counter}.jpeg`
+                    : `/360/jd_030664_spin_${counter}.jpeg`
+                }
                 alt=""
               />
             </div>
-            <svg
+            {/* <svg
               className="wave-1hkxOo"
               version="1.1"
               xmlns="http://www.w3.org/2000/svg"
@@ -61,7 +77,7 @@ export default function MostPopular() {
                 d="M826.337463,25.5396311 C670.970254,58.655965 603.696181,68.7870267 447.802481,35.1443383 C293.342778,1.81111414 137.33377,1.81111414 0,1.81111414 L0,150 L1920,150 L1920,1.81111414 C1739.53523,-16.6853983 1679.86404,73.1607868 1389.7826,37.4859505 C1099.70117,1.81111414 981.704672,-7.57670281 826.337463,25.5396311 Z"
                 fill="white"
               ></path>
-            </svg>
+            </svg> */}
           </section>
         </>
       );
