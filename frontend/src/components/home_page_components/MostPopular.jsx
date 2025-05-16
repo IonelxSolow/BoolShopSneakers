55387ac2cabb63f98ace033cac78d32c7266273e
NavodContrath <navod.contrath@gmail.com>
2025-05-16 17:27:40 +0200

    style: update styles for hot item section and improve responsiveness

--- frontend/src/components/home_page_components/MostPopular.jsx
+++ frontend/src/components/home_page_components/MostPopular.jsx
@@ -15,7 +15,7 @@ export default function MostPopular() {
 
         return 1;
       });
-    }, 100);
+    }, 200);
   }, []);
 
   switch (sneakers.state) {
@@ -53,17 +53,45 @@ export default function MostPopular() {
               fill="currentColor"
             ></path>
           </svg> */}
-          <section className=" p-0 container-fluid hot-item">
-            <div className="slider360 text-center">
-              <img
-                className="image360"
-                src={
-                  counter < 10
-                    ? `/360/jd_030664_spin_0${counter}.jpeg`
-                    : `/360/jd_030664_spin_${counter}.jpeg`
-                }
-                alt=""
-              />
+          <section className=" p-0 container hot-item">
+            <div className="row gap-3 align-items-baseline">
+              <div className="item-description text-end  col-12 col-lg-4 order-lg-2">
+                <div className="superbold-title mb-5">
+                  <span className="d-block newest-superbold" style={{ fontSize: "8rem", color: "red" }}>HOT</span>
+                  <span className="d-block newest-superbold ps-3">ITEM</span>
+                </div>
+                <div>
+                  <p className="item-title fs-2">{mostSoldSneaker.name}</p>
+                  <p className="item-brand fs-3 fw-bold">{mostSoldSneaker.brand}</p>
+                  <div className="d-flex align-items-center justify-content-end">
+                    <p className="item-price text-danger  fw-bold fs-3">{mostSoldSneaker.price}</p>
+                    <Link
+                      className="show-tag ms-3"
+                      to={"/product/air-force-1"}>Show More
+                    </Link>
+                  </div>
+                  <p className="item-description fs-4">
+                    Timeless Style. Everyday Comfort.
+                    Step into a legend with the Nike Air Force 1 Low  an icon that blends classic basketball heritage with modern streetwear appeal.
+                  </p>
+
+
+                </div>
+              </div>
+              <Link
+                className="slider360 text-center position-relative col-12 col-lg-7 order-lg-1"
+                to={"/product/air-force-1"}>
+                < img
+                  className="image360 img-fluid"
+                  src={
+                    counter < 10
+                      ? `/360/jd_030664_spin_0${counter}.jpeg`
+                      : `/360/jd_030664_spin_${counter}.jpeg`
+                  }
+                  alt=""
+                />
+              </Link>
+
             </div>
             {/* <svg
               className="wave-1hkxOo"
@@ -78,7 +106,7 @@ export default function MostPopular() {
                 fill="white"
               ></path>
             </svg> */}
-          </section>
+          </section >
         </>
       );
   }
