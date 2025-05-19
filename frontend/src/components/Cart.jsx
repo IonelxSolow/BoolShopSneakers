b6cc329ff77023d89c242c4035cd9defaed2bc27
alexciccarese <alex.ciccarese@gmail.com>
2025-05-19 16:46:59 +0200

    style: enhance Cart component layout for better mobile display; adjust structure and styling

--- frontend/src/components/Cart.jsx
+++ frontend/src/components/Cart.jsx
@@ -10,92 +10,94 @@ export default function Cart({ toggleCart, isOpen }) {
       className={`cart-content d-flex flex-column cart-fullscreen-mobile ${isOpen ? "open" : ""
         }`}
     >
-      <div className="d-flex justify-content-between align-items-center mb-4">
-        <button className="btn close-cart " onClick={toggleCart}>
-          <i className="d-flex bi bi-x"></i>
-        </button>
-        <span>
-          <Link to="/cart" className="nav-link cart-link">
-            View the cart
-          </Link>
-        </span>
-      </div>
+      <div className="container">
+        <div className="d-flex justify-content-between align-items-center mb-4">
+          <button className="btn close-cart " onClick={toggleCart}>
+            <i className="d-flex bi bi-x"></i>
+          </button>
+          <span>
+            <Link to="/cart" className="nav-link cart-link">
+              View the cart
+            </Link>
+          </span>
+        </div>
 
-      <h5 className="mt-3 mb-3">Your Cart</h5>
+        <h5 className="mt-3 mb-3">Your Cart</h5>
 
-      <div className="cart-items mt-3">
-        {cart.length > 0 ? (
-          cart.map((item) => (
-            <div
-              key={item.sku}
-              className="cart-item d-flex justify-content-between align-items-center mb-3"
-            >
-              <div className="d-flex flex-column flex-row align-items-start">
-                {/* Cerchietto con immagine */}
-                <div className="d-flex align-items-center">
-                  <div
-                    className="me-3"
-                    style={{
-                      width: "70px",
-                      height: "70px",
-                      borderRadius: "20%",
-                      overflow: "hidden",
-                      flexShrink: 0,
-                      boxShadow: "0 4px 16px 0 rgba(177, 177, 177, 0.15)"
-
-                    }}
-                  >
-                    <img
-                      src={`/assets/${item.image}`}
-                      alt={item.name}
+        <div className="cart-items mt-3">
+          {cart.length > 0 ? (
+            cart.map((item) => (
+              <div
+                key={item.sku}
+                className="cart-item d-flex justify-content-between align-items-center mb-3"
+              >
+                <div className="d-flex flex-column flex-row align-items-start">
+                  {/* Cerchietto con immagine */}
+                  <div className="d-flex align-items-center">
+                    <div
+                      className="me-3"
                       style={{
-                        width: "100%",
-                        height: "100%",
-                        objectFit: "cover",
+                        width: "70px",
+                        height: "70px",
+                        borderRadius: "20%",
+                        overflow: "hidden",
+                        flexShrink: 0,
+                        boxShadow: "0 4px 16px 0 rgba(177, 177, 177, 0.15)"
+
                       }}
-                    />
+                    >
+                      <img
+                        src={`/assets/${item.image}`}
+                        alt={item.name}
+                        style={{
+                          width: "100%",
+                          height: "100%",
+                          objectFit: "cover",
+                        }}
+                      />
+                    </div>
+
+                    <div className="d-flex flex-column justify-content-between h-100">
+                      <span style={{ maxWidth: "150px" }}>{item.name}</span>
+                      <IncreaseDecrease item={item} />
+                    </div>
                   </div>
 
-                  <div className="d-flex flex-column justify-content-between h-100">
-                    <span style={{ maxWidth: "150px" }}>{item.name}</span>
-                    <IncreaseDecrease item={item} />
-                  </div>
                 </div>
 
+                <div>
+                  <span>{Number(item.price).toFixed(2)} €</span>
+                  <button
+                    className="btn btn-sm btn-danger ms-3"
+                    onClick={() => removeItem(item.sku)}
+                  >
+                    <i className="bi bi-trash"></i>
+                  </button>
+                </div>
               </div>
+            ))
+          ) : (
+            <p>Your cart is empty</p>
+          )}
+        </div>
 
-              <div>
-                <span>{Number(item.price).toFixed(2)} €</span>
-                <button
-                  className="btn btn-sm btn-danger ms-3"
-                  onClick={() => removeItem(item.sku)}
-                >
-                  <i className="bi bi-trash"></i>
-                </button>
-              </div>
-            </div>
-          ))
-        ) : (
-          <p>Your cart is empty</p>
-        )}
-      </div>
+        <hr />
 
-      <hr />
-
-      {/* Totale */}
-      <div>
-        <h6 className="d-flex justify-content-between">
-          <span>Total:</span>
-          <span>{total.toFixed(2)}€</span>
-        </h6>
-        <div className="d-flex align-items-center">
-          <Link
-            to="/checkout"
-            className={`btn btn-main-light mt-3 mb-3 w-100 ${cart.length > 0 ? "" : "disabled"
-              }`}
-          >
-            Go to checkout
-          </Link>
+        {/* Totale */}
+        <div>
+          <h6 className="d-flex justify-content-between">
+            <span>Total:</span>
+            <span>{total.toFixed(2)}€</span>
+          </h6>
+          <div className="d-flex align-items-center">
+            <Link
+              to="/checkout"
+              className={`btn btn-main-light mt-3 mb-3 w-100 ${cart.length > 0 ? "" : "disabled"
+                }`}
+            >
+              Go to checkout
+            </Link>
+          </div>
         </div>
       </div>
     </div>
