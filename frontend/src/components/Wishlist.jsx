0a2e06aada93dd6475ac8594178c9ad97cfee458
alexciccarese <alex.ciccarese@gmail.com>
2025-05-19 16:48:00 +0200

    style: improve Wishlist component layout for better mobile display; adjust structure and styling

--- frontend/src/components/Wishlist.jsx
+++ frontend/src/components/Wishlist.jsx
@@ -12,66 +12,68 @@ export default function Wishlist({ isOpen, toggleWishlist }) {
       className={`cart-content d-flex flex-column cart-fullscreen-mobile ${isOpen ? "open" : ""
         }`}
     >
-      <div className="d-flex justify-content-between align-items-center mb-4">
-        <button className="btn close-cart " onClick={toggleWishlist}>
-          <i className="d-flex bi bi-x"></i>
-        </button>
-        <span>
-          <Link to="/wishlist" className="nav-link cart-link">
-            View the wishlist
-          </Link>
-        </span>
-      </div>
+      <div className="container">
+        <div className="d-flex justify-content-between align-items-center mb-4">
+          <button className="btn close-cart " onClick={toggleWishlist}>
+            <i className="d-flex bi bi-x"></i>
+          </button>
+          <span>
+            <Link to="/wishlist" className="nav-link cart-link">
+              View the wishlist
+            </Link>
+          </span>
+        </div>
 
-      <h5 className="mt-3 mb-3">Your Whishlist</h5>
+        <h5 className="mt-3 mb-3">Your Whishlist</h5>
 
-      <div className="cart-items mt-3">
-        {wishlist.length > 0 ? (
-          wishlist.map((item) => (
-            <div
-              key={item.sku}
-              className="cart-item d-flex justify-content-between align-items-center mb-3"
-            >
-              <div className="d-flex flex-column flex-row align-items-start">
-                {/* Cerchietto con immagine */}
-                <div className="d-flex align-items-center">
-                  <div
-                    className="me-3"
-                    style={{
-                      width: "70px",
-                      height: "70px",
-                      borderRadius: "20%",
-                      overflow: "hidden",
-                      flexShrink: 0,
-                    }}
-                  >
-                    <img
-                      src={`/assets/${item.image}`}
-                      alt={item.name}
+        <div className="cart-items mt-3">
+          {wishlist.length > 0 ? (
+            wishlist.map((item) => (
+              <div
+                key={item.sku}
+                className="cart-item d-flex justify-content-between align-items-center mb-3"
+              >
+                <div className="d-flex flex-column flex-row align-items-start">
+                  {/* Cerchietto con immagine */}
+                  <div className="d-flex align-items-center">
+                    <div
+                      className="me-3"
                       style={{
-                        width: "100%",
-                        height: "100%",
-                        objectFit: "cover",
+                        width: "70px",
+                        height: "70px",
+                        borderRadius: "20%",
+                        overflow: "hidden",
+                        flexShrink: 0,
                       }}
-                    />
-                  </div>
+                    >
+                      <img
+                        src={`/assets/${item.image}`}
+                        alt={item.name}
+                        style={{
+                          width: "100%",
+                          height: "100%",
+                          objectFit: "cover",
+                        }}
+                      />
+                    </div>
 
-                  <div className="d-flex flex-column justify-content-between h-100">
-                    <span style={{ maxWidth: "150px" }}>{item.name}</span>
+                    <div className="d-flex flex-column justify-content-between h-100">
+                      <span style={{ maxWidth: "150px" }}>{item.name}</span>
+                    </div>
                   </div>
                 </div>
+                <div>
+                  <i className={isDeleted === item.sku ? "bi bi-heartbreak-fill delete-wish text-danger fs-4 fs-md-2 ms-3" : "bi bi-heartbreak delete-wish text-danger fs-4 fs-md-2 ms-3"}
+                    onClick={() => removeFromWishlist(item.sku)}
+                    onMouseOver={() => { setisDeleted(item.sku) }}
+                    onMouseLeave={() => { setisDeleted("") }}></i>
+                </div>
               </div>
-              <div>
-                <i className={isDeleted === item.sku ? "bi bi-heartbreak-fill delete-wish text-danger fs-4 fs-md-2 ms-3" : "bi bi-heartbreak delete-wish text-danger fs-4 fs-md-2 ms-3"}
-                  onClick={() => removeFromWishlist(item.sku)}
-                  onMouseOver={() => { setisDeleted(item.sku) }}
-                  onMouseLeave={() => { setisDeleted("") }}></i>
-              </div>
-            </div>
-          ))
-        ) : (
-          <p>Your Whishlist is empty</p>
-        )}
+            ))
+          ) : (
+            <p>Your Whishlist is empty</p>
+          )}
+        </div>
       </div>
     </div>
   );
